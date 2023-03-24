9fb5c8a0501da23026971a403c45f46cd0f99d50
Marko2353 <57666712+Marko2353@users.noreply.github.com>
2023-03-24 18:27:09 +0100

    Fix

---
+++ app/locations/page.tsx
@@ -0,0 +1,7 @@
+export default function Locations() {
+  return (
+    <main>
+      <span>Hello</span>
+    </main>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,9 @@
-"use client";
-
-import type { RootState } from "./GlobalRedux/store";
-import { useSelector, useDispatch } from "react-redux";
-import {
-  increment,
-  decrement,
-  incrementByAmount,
-} from "./GlobalRedux/Features/counter/counterSlice";
+import Locations from "./locations/page";
 
 export default function Home() {
-  const count = useSelector((state: RootState) => state.counter.value);
-  const dispatch = useDispatch();
-
   return (
     <main>
-      <button className="bg-gray-500" onClick={() => dispatch(increment())}>
-        Increment
-      </button>
-      <span className="font-bold">{count}</span>
-      <button onClick={() => dispatch(decrement())}>Decrement</button>
-      <button onClick={() => dispatch(incrementByAmount(2))}>
-        Increment by 2
-      </button>
+      <Locations />
     </main>
   );
 }
